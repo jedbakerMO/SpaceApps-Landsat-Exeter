15fc9e37784012827e51b6e2f459a4c8808520ac
manith-dell <manithhetti@gmail.com>
2024-10-06 15:57:27 +0100

    changed app.py

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 import os
 
 app = Flask(__name__)
 
-from routes.home import *
+@app.route("/")
+def home():
+    return render_template('index.html')
 
+@app.route("/docs")
+def docs():
+    return render_template('docs.html')
 
 
 
 if __name__ == "__main__":
   port = int(os.environ.get('PORT', 3030))
   app.run(debug=True, host='0.0.0.0', port=port)
-
